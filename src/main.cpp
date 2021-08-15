84f896ad9524deed8f9146949d74c4ae8460b613
k4-100 <k4-100@protonmail.com>
2021-08-15 13:10:19 +0200

    Added commandline arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,26 @@
 #include "include/utl.hpp"
 
-int main()
+int main( int argc, char *argv[] )
 {   
-    const std::string PATH =  "./release/test.txt";
-    std::cout << "compiled\n";
+    std::cout<< "argc: "<< argc<<'\n';
+    std::cout<< "argv: "<< argv[ 0 ]<<'\n';
 
-    std::string str = CMPS::read_from_file( PATH );
 
-    // CMPS::write_to_file( PATH, ( str + str ) );
-    // const std::string source = "aaaaaabbccccccc21421521125";
-    const std::string comppressed = CMPS::compress( str );
+    // const std::string PATH =  "./release/test.txt";
+    // std::cout << "compiled\n";
+
+    // std::string str = CMPS::read_from_file( PATH );
+
+    // // CMPS::write_to_file( PATH, ( str + str ) );
+    // // const std::string source = "aaaaaabbccccccc21421521125";
+    // const std::string comppressed = CMPS::compress( str );
     
 
-    std::cout << "str: " << ": " << str.size() << '\n';
-    std::cout<< "\n#####################\n\n";
-    std::cout << comppressed << ": " << comppressed.size() << '\n';
+    // std::cout << "str: " << ": " << str.size() << '\n';
+    // std::cout<< "\n#####################\n\n";
+    // std::cout << comppressed << ": " << comppressed.size() << '\n';
     
-    std::cout<< "\n#####################\n\n";
-    const std::string decompressed =  CMPS::decompress( comppressed );
-    std::cout << decompressed << ": " << decompressed.size() << '\n';
+    // std::cout<< "\n#####################\n\n";
+    // const std::string decompressed =  CMPS::decompress( comppressed );
+    // std::cout << decompressed << ": " << decompressed.size() << '\n';
 }
