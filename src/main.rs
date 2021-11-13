632b7d15dd84af31648218614a2ec7a4c8ca05f8
k4-100 <k4-100@protonmail.com>
2021-11-13 17:00:44 +0100

    Testing writing into files

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     // let mut input = String::new();
     // std::io::stdin().read_line( &mut input ).unwrap();
 
-    let text : String = fs::read_to_string("test.txt").expect("shit broke");
-
-    println!("{}",text);
+    let text : String = fs::read_to_string("res/test.txt").expect("shit broke");
+    fs::write("test2.txt", &text).expect("writing broke");
+    println!("{}", &text);
 }
