239c7eec3e1f067e868845521fef1bfc859d5fa2
k4-100 <k4-100@protonmail.com>
2021-11-13 16:18:04 +0100

    Added read_from_file function

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ fn main() {
     let mut input = String::new();
     std::io::stdin().read_line( &mut input ).unwrap();
 
-    println!( "{}",input );
+    utl::read_from_file( &input );
 }
--- src/utl.rs
+++ src/utl.rs
@@ -1,10 +1,4 @@
-/// write data to a file from array,
-/// where each array index is one line
-pub fn write_array_to_file(){
-    println!("werks");
+/// read from file
+pub fn read_from_file( path : &str ){
+    println!("{}",path);
 }
-
-// generate test content
-// pub fn generate_test_content(){
-
-// }
