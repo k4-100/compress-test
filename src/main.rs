55a66a30ecf07c574b013926b37af09a0205c964
k4-100 <k4-100@protonmail.com>
2021-11-13 21:28:55 +0100

    Started working on generate_file function

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,8 @@
 // use std::fs;
+// use rand::Rng;
 
-use rand::Rng;
 mod utl;
 
 fn main() {
-    // let mut input = String::new();
-    // std::io::stdin().read_line( &mut input ).unwrap();
-
-    // let text : String = fs::read_to_string("res/test.txt").expect("shit broke");
-    // fs::write("test2.txt", &text).expect("writing broke");
-    // println!("{}", &text);
-
-    let mut v: Vec< &str > = vec![];
-
-    v.push( "char ain't here" );
-
-
-
-    (0..30).for_each( |_|  println!("{:?}", &rand::thread_rng().gen_range(15..(30+1)) ) );
-        
-    
-    
+    utl::generate_file( 100 );
 }
---
+++ src/utl.rs
@@ -0,0 +1,10 @@
+use rand::Rng;
+
+pub fn generate_file( lines : u32 ){
+    let v : Vec<String> = vec![]; 
+    
+
+    for i in (0..lines){
+        println!("{}",i);
+    }
+}
