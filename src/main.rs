ae9568f0aef95b913b5c15ee6612aea088e2203a
k4-100 <k4-100@protonmail.com>
2021-12-16 17:21:41 +0100

    Added debug to rust

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,12 @@
 mod utl;
 
 fn main() {
-    let big_string : Vec<String> = utl::generate_file( 1 );
+    let big_string : Vec<String> = utl::generate_file( 100 );
     
+    let mut a : i32 = 0;
+    a+= 10;
+    a+= 30;
+
 
     utl::write_into_file( big_string );
 
