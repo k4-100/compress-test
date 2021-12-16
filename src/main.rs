9a362c03d3aeac3382221ef615828265b2909795
k4-100 <k4-100@protonmail.com>
2021-12-16 17:15:53 +0100

    purged most of things

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,9 @@
 mod utl;
 
 fn main() {
-    let big_string : Vec<String> = utl::generate_file( 1000 );
+    let big_string : Vec<String> = utl::generate_file( 1 );
     
 
     utl::write_into_file( big_string );
 
-
-    // let big_string : Vec<String> = utl::read_from_file_to_vector();
-
-    // utl::compress_file( big_string );
-
-    // println!("{:?}",big_string);
-
-    // utl::write_into_file( big_string );
-
-    // let s  = "a".to_string() + "sda" + "sa";
-   
-    // println!("{}", s);
-
-    // let read_vec = utl::read_from_file_to_vector();
-
-    // println!("{}", read_vec.len());
-   
-    
-
-    // .split("a").collect::<Vec<&str>>();
-
-    // println!("{:?}", content);
 }
